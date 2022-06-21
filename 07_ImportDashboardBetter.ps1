567347e401a08bdf468d9a396320b1b8ecd69c46
kmsigma <kevin.sparenberg@solarwinds.com>
2022-06-21 11:55:12 -0400

    Added check for Import/Export function

--- 07_ImportDashboardBetter.ps1
+++ 07_ImportDashboardBetter.ps1
@@ -6,8 +6,17 @@ if ( -not $SwisConnection ) {
 
 # Import the Modern Dashboard functions
 # Download from: https://github.com/solarwinds/OrionSDK/blob/master/Samples/PowerShell/functions/func_ModernDashboards.ps1
-# and save on your local machine.
-. .\func_ModernDashboards.ps1
+if ( -not ( Test-Path -Path '.\functions\' -ErrorAction SilentlyContinue ) ) {
+    # We don't have the directory, so let's create it.
+    New-Item -Path '.\functions\' -ItemType Directory | Out-Null
+}
+if ( -not ( Test-Path -Path '.\functions\func_ModernDashboards.ps1' -ErrorAction SilentlyContinue ) ) {
+    # We don't have the functions, so let's download them
+    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/solarwinds/OrionSDK/master/Samples/PowerShell/functions/func_ModernDashboards.ps1' -OutFile '.\functions\func_ModernDashboards.ps1'
+}
+    
+# Now that we have the functions, let's call them to load them into memory.
+. .\functions\func_ModernDashboards.ps1
 
 # Import the modern dashboard
 Import-ModernDashboard -SwisConnection $SwisConnection -Path '.\Scratch\ImportThisOtherDashboard.json'
